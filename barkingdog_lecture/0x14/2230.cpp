99c99485d67b3f9011290a5dcf5b432acdacf1f4
kaeba0616 <alstjq1012@gmail.com>
2022-11-19 10:34:32 +0900

    Update 2230.cpp

---
+++ barkingdog_lecture/0x14/2230.cpp
@@ -0,0 +1,30 @@
+//수 고르기
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int a[100005];
+int mn = 0x7fffffff;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) cin >> a[i];
+  sort(a, a + n);
+  int en = 0;
+  for (int st = 0; st < n; st++) {
+    while (en < n && a[en] - a[st] < m) en++;
+    if (en == n) break;
+    mn = min(mn, a[en] - a[st]);
+  }
+  cout << mn;
+}
+
+/*
+for(int i=0; i<n; i++)
+  for(int j =0; j<n; j++)
+    if(a[j] - a[i] >= m)
+      ans = min(ans, a[j]-a[i]);
+*/
