d552ea3b24c2fd6b6aa947b48da64172299b6d7a
kaeba0616 <alstjq1012@gmail.com>
2022-11-22 10:03:58 +0900

    algorithm 11-22

---
+++ seoultech/introduce_cpp/lecture/inheritance/practice_1/salariedemployee.h
@@ -0,0 +1,8 @@
+// this is the header file salariedemployee.h
+// this is the interface for the class SalariedEmployee
+#ifndef SALARIEDEMPLOYEE_H
+#define SALARIEDEMPLOYEE_H
+
+#include<string>
+#include "employee.h"
+
