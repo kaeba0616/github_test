fa89192694d644d73f361de5b7a9184084e6fb60
kaeba0616 <alstjq1012@gmail.com>
2022-11-28 10:02:08 +0900

    11-28

---
+++ seoultech/introduce_cpp/lecture/virtual function basics/salemain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "sale.h"
+#include "discountsale.h"
+using std::cout;
+using std::endl;
+using std::ios;
+using namespace SavitchSale;
+
+int main(){
+  Sale simple(10.00);
+  DiscountSale discount(11.00, 10);
+
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(2);
+
+  if(discount <simple){
+    cout << "Discounted item is cheaper.\n";
+    cout << "Savings is $ " << simple.savings(discount)<<"\n";
+  }
+  else{
+    cout << "Discounted item is not cheaper.\n";
+  }
+
+  return 0;
+}
+
+19:00
